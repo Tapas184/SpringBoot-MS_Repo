0da4996b8733f1dce05e6aac7daeb6b522352669
user <user@TapasRout>
2023-01-19 20:43:28 +0530

    Modify 2nd time

---
+++ IOCProj-2RealtimeDI-RealtimeStratageDP/src/main/java/com/nt/service/CustomerService.java
@@ -0,0 +1,13 @@
+package com.nt.service;
+
+import com.nt.dto.CustomerDTO;
+
+public class CustomerService implements ICustomerService {
+
+
+    public String calculateSimpleIntrest(CustomerDTO dto) throws Exception {
+	float intAmount = ;
+	return null;
+    }
+
+}
