a44f11c843381103c7aaddabe5f8c8ffb1940a94
user <user@TapasRout>
2023-02-23 10:05:06 +0530

    Spring profile,yml and properties examples with mini project

---
+++ SpringBootProject-08-SpringProfiles/src/main/java/com/nt/vo/EmployeeVo.java
@@ -0,0 +1,14 @@
+//Employee Vo(Value Object) class
+package com.nt.vo;
+
+import lombok.Data;
+
+@Data
+public class EmployeeVo {
+	private String slNumber;
+	private String empNo;
+	private String ename;
+	private String sal;
+	private String job;
+
+}//class
