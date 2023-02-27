e941e886129f2e38534bcef34137f03bd662b1dd
user <user@TapasRout>
2023-02-27 10:09:54 +0530

    Spring JPA Paging

---
+++ SpringBootJPAProject-03-PagingAndShorting/src/main/java/com/nt/service/ICoronaVaccineMgmtService.java
@@ -0,0 +1,17 @@
+//
+package com.nt.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.nt.entity.CoronaVacine;
+
+public interface ICoronaVaccineMgmtService {
+	public Iterable<CoronaVacine> fatchDetailsBy(boolean asc,String... props);
+	
+	//by page
+	public Iterable<CoronaVacine> fatchDetailsByPageNo(int pageNo,int pageSize,boolean asc,String...properties);
+	
+	//page by page(PageNation)
+	//public void fatchByPageNation(int pageSize);
+}
